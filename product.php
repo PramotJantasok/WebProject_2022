4ddfa5d96dc4c63ec37a66024d899a7c1546a8e0
Wichai <wichailoveyou3000@gmail.com>
2022-11-19 13:25:12 +0700

    BIG

--- product.php
+++ product.php
@@ -11,8 +11,9 @@
     $imgPage = $data[$_GET['book']]->img;
     $groupPage = $data[$_GET['book']]->group;
     $productID = $data[$_GET['book']]->id;
-    $bookWriter = $data[$_GET['book']]->Wirter;
+    $bookWriter = $data[$_GET['book']]->Writer;
     $Price = $data[$_GET['book']]->price;
+    $Discount = $data[$_GET['book']] -> discount;
     $Detali = "";
 
     foreach ($data[$_GET['book']]->detail as $mes){
@@ -32,10 +33,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./css_file/index.css">
     <link rel="stylesheet" href="./css_file/slider.css">
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/all.min.css?ver=5.13.3' />
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
     <?php echo '<title>' . $namePage . '</title>'; ?>
+    <style>
+        .price-wrapper {
+            font-size: 20px;
+            right: 10px;
+            color: grey;
+            margin-top: 2px;
+        }
+        .dis{
+            font-size: 20px;
+        }
+    </style>
 </head>
 
 <body>
@@ -55,39 +67,76 @@
 
     <div class="container py-4">
         <div class="row py-4">
-<?php 
-        echo '<div class="col-xxl-5 col-xl-5 col-lg-6 col-md-7 col-sm-12 py-4 ">';
-        echo    '<img src="'.$imgPage.'" class="img-fluid" />';
-        echo '</div>';
-        echo '<div class="col-xxl-7 col-xl-7 col-lg-6 col-md-5 col-sm-12 py-4 ">';
-        echo    '<div class="row py-4">';
-        echo        '<div class="col py-2">';
-        echo            '<h3>'.$namePage.'</h3>';
-        echo            '<p>'.$bookWriter.'</p>';
-        echo         '</div>';
-        echo     '</div>';
-        echo     '<div class="row">';
-        echo        '<div class="col">';
-        echo            '<h5>รหัสสินค้า : '.$productID.'</h5>';
-        echo            '<h5>หมวดหมู่ : '.$groupPage.'</h5>';
-        echo        '</div>';
-        echo     '</div>';
-        echo     '<div class="row">';
-        echo        '<div class="col-xxl-3 col-xl-2 col-lg-4 col-md-4 col-3 py-4 ">';
-        echo            '<div class="input-group">';
-        echo                '<button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="bi bi-dash"></i></button>';
-        echo                '<input type="text" class="form-control text-center" placeholder="" aria-describedby="button-addon2" value="1" disabled>';
-        echo                '<button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="bi bi-plus"></i></button>';
-        echo            '</div>';
-        echo        '</div>';
-        echo     '</div>';
-        echo    '<div class="row">';
-        echo        '<div class="col">';
-        echo            '<p>'.$Detali.'</p>';
-        echo        '</div>';
-        echo     '</div>';
-        echo '</div>';
-?>    
+         <div class="col-xxl-5 col-xl-5 col-lg-6 col-md-7 col-sm-12 py-4 ">
+            <?php echo '<img src="'.$imgPage.'" class="img-fluid" />'; ?>
+         </div>
+         <div class="col-xxl-7 col-xl-7 col-lg-6 col-md-5 col-sm-12 py-4 ">
+            <div class="row py-4">
+                <div class="col py-2">
+                    <?php echo '<h3>' .$namePage. '</h3>'; ?>
+                    <?php echo '<p>'. $bookWriter .'</p>'; ?>
+                 </div>
+             </div>
+             <div class="row">
+                <div class="col">
+                    <?php echo '<h5>รหัสสินค้า : '.$productID.'</h5>'; ?>
+                    <?php echo '<h5>หมวดหมู่ : '.$groupPage.'</h5>' ;?>
+                </div>
+             </div>
+            <!-- <div class="row py-4">
+                <div class="col-xxl-3 col-xl-2 col-lg-4 col-md-4 col-3 py-4 ">
+                    <div class="input-group">
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="bi bi-dash"></i></button>
+                        <input type="text" class="form-control text-center" placeholder="" aria-describedby="button-addon2" value="1" disabled>
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="bi bi-plus"></i></button>
+                    </div>
+                </div>
+            </div> -->
+            <div class="row">
+                <div class="col">
+                    <?php echo '<p>'.$Detali.'</p>'; ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="price-wrapper">
+                <?php  if ($Discount > 0){
+                    echo '<h4>ราคา : <span class="price">';
+                    echo '<del>';
+                    echo '<span class="woocommerce-Price-amount amount dis">';
+                    echo '<bdi>';
+                    echo '<span class="woocommerce-Price-currencySymbol ">&#3647;</span> '. $Price.'';
+                    echo '</bdi>';
+                    echo '</span>';
+                    echo '</del>&nbsp;';
+                    // echo '<ins>';
+                    echo '<span class="woocommerce-Price-amount amount">';
+                    echo '<bdi>';
+                    echo '<span class="woocommerce-Price-currencySymbol">';
+                    echo '&#3647;';
+                    echo '</span>'. $Discount.'';
+                    echo '</bdi>';
+                    echo '</span>';
+                    // echo '</ins>';
+                    echo '</span> </h4>';
+                }else{
+                    echo '<div class="price-wrapper">';
+                    echo '<h4> ราคา : <span class="price">
+                    <span class="woocommerce-Price-amount amount">
+                    <span class="woocommerce-Price-currencySymbol">
+                    &#3647;
+                    </span>'. $Price .'</span></span> </h4>';      
+                    
+                }
+                    ?>
+                </div>
+            </div>
+            <div class="row py-4">
+                <div class="col">
+                <button type="button" class="btn btn-info btn-lg" style="color: #FFF;">หยิบใส่ตะกร้า</button>
+                </div>
+            </div>
+
+         </div>
         </div>
     </div>
 
@@ -103,3 +152,4 @@
 </body>
 
 </html>
+
